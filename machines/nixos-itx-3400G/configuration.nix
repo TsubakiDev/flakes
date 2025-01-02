63c5f076152415ffa3cb58c2e3af1c68ec44d4b6
TsubakiDev <i@tsubakidev.cc>
2025-01-02 15:58:14 +0800

    moze

--- machines/nixos-itx-3400G/configuration.nix
+++ machines/nixos-itx-3400G/configuration.nix
@@ -31,8 +31,8 @@
     type = "fcitx5";
     fcitx5.addons = with pkgs; [
       fcitx5-material-color
-      fcitx5-mozc
-      fcitx5-rime
+      fcitx5-chinese-addons
+      fcitx5-moze
       fcitx5-gtk
     ];
   };
