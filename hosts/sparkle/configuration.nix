714948ab6d20a6e410ceae62625976188dc00157
TsubakiDev <i@tsubakidev.cc>
2025-01-25 13:49:28 +0800

    feat: update

---
+++ hosts/sparkle/configuration.nix
@@ -0,0 +1,67 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ../../common
+    ../../services
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  networking.hostName = "sparkle";
+
+  time.timeZone = "Asia/Shanghai";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-material-color
+      fcitx5-rime
+      fcitx5-mozc
+      fcitx5-gtk
+    ];
+  };
+
+  users.users.tsubaki = {
+    isNormalUser = true;
+    description = "tsubaki";
+    uid = 1000;
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    shell = pkgs.fish;
+  };
+
+  programs = {
+    fish.enable = true;
+    dconf.enable = true;
+    gamemode.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    neovim
+    wget
+    git
+    gcc
+  ];
+
+  system.stateVersion = "24.11";
+}
--- modules/anime-games-launcher/default.nix
+++ modules/anime-games-launcher/default.nix
@@ -8,4 +8,5 @@
 
   nix.settings = inputs.aagl.nixConfig;
   programs.honkers-railway-launcher.enable = true;
+  programs.anime-game-launcher.enable = true;
 }
