a20d3c68c9599faf94a9b5087705aaaabe789ac6
Daniel Wang <i@tsubakidev.cc>
2025-03-16 11:42:12 +0800

    simplify

--- hosts/chronara/configuration.nix
+++ hosts/chronara/configuration.nix
@@ -1,5 +1,15 @@
-{ ... }:
+{ config, pkgs, ... }:
 {
+  imports = [
+    ./display.nix
+    ./sound.nix
+    ./dae
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  networking.hostName = "chronara";
+
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
@@ -11,7 +21,39 @@
     ];
   };
 
-  ix = {
+  time.timeZone = "Asia/Shanghai";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  users.users.tsubaki = {
+    isNormalUser = true;
+    description = "tsubaki";
+    uid = 1000;
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    shell = pkgs.fish;
+  };
+
+  environment.systemPackages = with pkgs; [
+    wget
+    gcc
+  ];
+
+  nix = {
     package = pkgs.lix;
 
     settings = {
@@ -29,4 +71,6 @@
       options = "--delete-older-than 1d";
     };
   };
+
+  system.stateVersion = "24.11";
 }
