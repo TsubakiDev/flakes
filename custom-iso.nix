991a4004da1967a0ecb71cf133a57ba74efdf139
Daniel Wang <i@tsubaki.dev>
2025-04-12 10:15:50 +0800

    use plasma

--- custom-iso.nix
+++ custom-iso.nix
@@ -1,10 +1,9 @@
 { pkgs, modulesPath, lib, ... }: {
   imports = [
-    "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
+    "${modulesPath}/installer/cd-dvd/installation-cd-graphical-calamares-plasma6.nix"
   ];
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.supportedFilesystems = lib.mkForce [ "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" ];
-
   nix.settings.substituters = lib.mkForce [ "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store" ];
 }
